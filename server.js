d4e56e576cb93746ca5c2817a0dd170d73368653
Kavish M <kavishm100@gmail.com>
2026-03-11 21:49:09 +0530

    Added SSL and Port for Aiven Database

--- server.js
+++ server.js
@@ -20,8 +20,11 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
-
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS }
